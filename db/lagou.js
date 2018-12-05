aa165a431fd97c46925826936321a305b171b7c2
TianchengLee <ltc6634284@gmail.com>
2018-12-05 18:40:24 +0800

    优化存储数据库

--- db/lagou.js
+++ db/lagou.js
@@ -3,6 +3,7 @@ const conn = require('./index.js')
 module.exports = {
   addData(data) {
     conn.query('select count(*) as count from lagou where companyId = ? limit 0, 1', data.companyId, (err, res) => {
+      if (err) return;
       if (res[0].count == 0) {
         conn.query('insert into lagou set ?', data)
       }
